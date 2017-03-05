390316dfa7665d90333547e76fb81b70612c4afa
karthikc17 <vijaykarthik52@gmail.com>
2017-03-05 16:52:43 +0530

    python

---
+++ cal.py
@@ -0,0 +1,17 @@
+op=input("option\n")
+a=input("enter the value a\n")
+b=input("enter the value b\n")
+
+
+if op==1:
+	print "add",(a+b)
+elif op==2:
+	print a-b
+elif op==3:
+	print a*b
+elif op==4:
+	print a/b
+elif op==5:
+	print a**b
+
+
